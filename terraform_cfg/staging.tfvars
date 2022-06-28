3ffc899a4640b32f814d68d33c3c1eaee01ddd2b
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2022-06-28 13:44:20 +0545

    added the configuration files for ecs, securitygrps, autoscaling, variables updated;

--- terraform_cfg/staging.tfvars
+++ terraform_cfg/staging.tfvars
@@ -4,3 +4,18 @@ environment = "staging"
 
 # vpc
 az_count = 2
+
+# app
+app_port = "80"
+
+# ECS role
+ecs_task_execution_role = "ECSTaskExecutionRole"
+
+# alb
+health_check_path = "/"
+
+# ECS
+fargate_cpu = "1024"
+fargate_memory = "2048"
+app_count = 2
+app_image = "nginx:latest"
--- terraform_cfg/variables.tf
+++ terraform_cfg/variables.tf
@@ -5,3 +5,17 @@ variable aws_profile {}
 variable az_count {}
 
 variable environment {}
+
+variable app_port {}
+
+variable "ecs_task_execution_role" {}
+
+variable health_check_path {}
+
+variable fargate_cpu {}
+
+variable fargate_memory {}
+
+variable app_count {}
+
+variable app_image {}
