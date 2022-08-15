e4477f04b03199281de1b355b0a4a4a704a39721
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-08-15 10:41:52 +0545

    Add database routing

--- scripts/run_develop.sh
+++ scripts/run_develop.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 python manage.py collectstatic --noinput &
-python manage.py migrate &
+# NOTE: We will only migrate data in django migration
+# Don't use default schmea for migrations
+python manage.py migrate --database=django &
 python manage.py runserver 0.0.0.0:7020
