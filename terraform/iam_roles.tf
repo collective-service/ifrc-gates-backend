28f6aac65d0823192b34a09a80cddd8d23208665
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2022-09-12 23:59:23 +0545

    added all configuration files;

---
+++ terraform/iam_roles.tf
@@ -0,0 +1,49 @@
+data "aws_iam_policy_document" "ecs_task_execution_role" {
+  version = "2012-10-17"
+  statement {
+    sid     = ""
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "${var.ecs_task_execution_role}-${var.environment}"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy" "param_store" {
+  name = "secrets-paramstore"
+  role = aws_iam_role.ecs_task_execution_role.id
+  policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": [
+          "ssm:GetParameters"
+        ],
+        "Effect": "Allow",
+        "Resource": [
+          "${data.aws_ssm_parameter.dbname.arn}",
+          "${data.aws_ssm_parameter.dbuser.arn}",
+          "${data.aws_ssm_parameter.dbpwd.arn}",
+          "${data.aws_ssm_parameter.dbhost.arn}",
+          "${data.aws_ssm_parameter.dbport.arn}",
+          "${data.aws_ssm_parameter.secret_key.arn}"
+        ]
+      }
+    ]
+  }
+  EOF
+}
---
+++ terraform/securitygrps.tf
@@ -0,0 +1,39 @@
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-security-group-${var.environment}"
+  description = "Controls access to the ALB"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = var.app_port
+    to_port     = var.app_port
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ecs_sg" {
+  name        = "ecs-tasks-security-group-${var.environment}"
+  description = "allow inbound access from the ALB only"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = var.app_port
+    to_port         = var.app_port
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
