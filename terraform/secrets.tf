69912d2019e49a9d61f3d5106d89b95891d1774f
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2022-09-14 21:43:56 +0545

    removed data from secrets manager; instead use ssm parameters;

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,41 +1,5 @@
-data "aws_secretsmanager_secret" "ecr_backend_url" {
-    name = var.backend_ecr_secret_name
-}
-
-data "aws_secretsmanager_secret_version" "ecr_backend_url" {
-    secret_id = data.aws_secretsmanager_secret.ecr_backend_url.id
-}
-
-data "external" "ecr_backend" {
-    program = ["echo", "${data.aws_secretsmanager_secret_version.ecr_backend_url.secret_string}"]
-}
-
-data "aws_secretsmanager_secret" "postgres" {
-    name = var.postgres_secret_name
-}
-
-data "aws_secretsmanager_secret_version" "postgres" {
-    secret_id = data.aws_secretsmanager_secret.postgres.id
-}
-
-data "external" "postgres_creds" {
-    program = ["echo", "${data.aws_secretsmanager_secret_version.postgres.secret_string}"]
-}
-
-data "aws_secretsmanager_secret" "django" {
-    name = var.django_secret_name
-}
-
-data "aws_secretsmanager_secret_version" "django" {
-    secret_id = data.aws_secretsmanager_secret.django.id
-}
-
-data "external" "django" {
-    program = ["echo", "${data.aws_secretsmanager_secret_version.django.secret_string}"]
-}
-
-data "aws_ssm_parameter" "ecr_image_url" {
-    name = "ecr_image_url"
+data "aws_ssm_parameter" "ecr_backend_image_url" {
+    name = "ecr_backend_image_url"
 }
 
 data "aws_ssm_parameter" "dbhost" {
