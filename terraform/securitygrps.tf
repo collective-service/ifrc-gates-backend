a4da08cace9b6a0e9cdd4102d5eccb2c7bfc8295
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2022-09-30 11:12:22 +0545

    fixed on ingress policy;

--- terraform/securitygrps.tf
+++ terraform/securitygrps.tf
@@ -41,10 +41,10 @@ resource "aws_security_group" "ecs_sg" {
     protocol        = "tcp"
     from_port       = var.app_port
     to_port         = var.app_port
-    security_groups = [
-        aws_security_group.alb_sg.id,
-        "0.0.0.0/0"
-    ]
+    cidr_blocks     = ["0.0.0.0/0"]
+    # security_groups = [
+    #     aws_security_group.alb_sg.id
+    # ]
   }
 
   egress {
