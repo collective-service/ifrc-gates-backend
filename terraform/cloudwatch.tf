b9410059f6a462ab4ea0459b0d94d873e6d418e4
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2022-12-29 10:36:40 +0545

    updated the log-stream ref name;

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -7,7 +7,7 @@ resource "aws_cloudwatch_log_group" "log_group" {
   }
 }
 
-resource "aws_cloudwatch_log_stream" "myapp_log_stream" {
+resource "aws_cloudwatch_log_stream" "log_stream" {
   name           = "log-stream-backend-${var.environment}"
   log_group_name = aws_cloudwatch_log_group.log_group.name
 }
